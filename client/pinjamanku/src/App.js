afa0c20a2c10e12c5872af8de3cc7aa2e81ee56c
KuraoHikari <dewaindra795@gmail.com>
2021-09-22 21:20:55 +0800

    done

--- client/pinjamanku/src/App.js
+++ client/pinjamanku/src/App.js
@@ -20,27 +20,49 @@ import {
 import AddLoan from "./pages/Lender/formAddLoan";
 import store from "./store";
 export default function App() {
-  console.log(localStorage.getItem("access_token"));
+  // console.log(localStorage.getItem("access_token"));
+  // console.log(localStorage.getItem("role"));
   const Guard = (to, from, next) => {
-    console.log(to);
+    if (localStorage.getItem("role") === "admin" && to.meta.name === "admin") {
+      next();
+    }
+    if (localStorage.getItem("role") !== "admin" && to.meta.name === "admin") {
+      next.redirect("/");
+    }
+    if (localStorage.getItem("access_token") && to.meta.name === "register") {
+      next.redirect("/");
+    }
+    if (localStorage.getItem("access_token") && to.meta.name === "login") {
+      next.redirect("/");
+    }
     if (
-      localStorage.getItem("access_token") &&
-      to.match.path === "/register"
+      localStorage.getItem("role") === "lender" &&
+      to.meta.name === "borrower"
     ) {
       next.redirect("/");
     }
-    if (localStorage.getItem("access_token") && to.match.path === "/login") {
+    if (
+      localStorage.getItem("role") === "borrower" &&
+      to.meta.name === "lender"
+    ) {
       next.redirect("/");
     }
+    if (
+      localStorage.getItem("role") === "borrower" &&
+      to.meta.name === "addLoan"
+    ) {
+      next.redirect("/");
+    }
+    if (!localStorage.getItem("access_token") && to.meta.name === "addLoan") {
+      next.redirect("/login");
+    }
+    if (!localStorage.getItem("access_token") && to.meta.name === "lender") {
+      next.redirect("/login");
+    }
+    if (!localStorage.getItem("access_token") && to.meta.name === "borrower") {
+      next.redirect("/login");
+    }
     next();
-    // if (localStorage.getItem("access_token")) {
-    //   console.log("masuk");
-    //   next("/login");
-    // }
-    // if (localStorage.getItem("access_token") && to === "/register") {
-    //   next().redirect("/");
-    // }
-    // next();
   };
   return (
     <div>
@@ -48,41 +70,67 @@ export default function App() {
         <BrowserRouter>
           <GuardProvider guards={Guard} loading={""} error={""}>
             <Switch>
-              <Route path="/admin-dashboard/borrowerDetail/:userId">
-                <DetailBorrower />
-              </Route>
-              <Route path="/admin-dashboard/borrower">
-                <ListUserBorrower />
-              </Route>
-              <Route path="/admin-dashboard/lender">
-                <ListUserLender />
-              </Route>
-              <Route path="/admin-dashboard/pendingborrower">
-                <ListUserPendingBorrower />
-              </Route>
-              <Route path="/admin-dashboard/listLoan">
-                <ListLoan />
-              </Route>
-              <Route path="/lender/add-loan">
-                <AddLoan />
-              </Route>
-              <Route path="/lender">
-                <Lender />
-              </Route>
-              <Route path="/borrower">
-                <DasboardBorower />
-              </Route>
+              <GuardedRoute
+                path="/admin-dashboard/borrowerDetail/:userId"
+                component={DetailBorrower}
+                meta={{ name: "admin" }}
+              />
+
+              <GuardedRoute
+                path="/admin-dashboard/borrower"
+                exact
+                component={ListUserBorrower}
+                meta={{ name: "admin" }}
+              />
+
+              <GuardedRoute
+                path="/admin-dashboard/lender"
+                exact
+                component={ListUserLender}
+                meta={{ name: "admin" }}
+              />
+
+              <GuardedRoute
+                path="/admin-dashboard/pendingborrower"
+                exact
+                component={ListUserPendingBorrower}
+                meta={{ name: "admin" }}
+              />
+              <GuardedRoute
+                path="/admin-dashboard/listLoan"
+                exact
+                component={ListLoan}
+                meta={{ name: "admin" }}
+              />
+              <GuardedRoute
+                path="/lender/add-loan"
+                exact
+                component={AddLoan}
+                meta={{ name: "addLoan" }}
+              />
+              <GuardedRoute
+                path="/lender"
+                exact
+                component={Lender}
+                meta={{ name: "lender" }}
+              />
+              <GuardedRoute
+                path="/borrower"
+                exact
+                component={DasboardBorower}
+                meta={{ name: "borrower" }}
+              />
               <GuardedRoute
                 path="/register"
                 exact
                 component={Register}
-                meta={{ auth: true }}
+                meta={{ name: "register" }}
               />
               <GuardedRoute
                 path="/login"
                 exact
                 component={Login}
-                meta={{ auth: true }}
+                meta={{ name: "login" }}
               />
 
               <GuardedRoute path="/" exact component={Home} />
